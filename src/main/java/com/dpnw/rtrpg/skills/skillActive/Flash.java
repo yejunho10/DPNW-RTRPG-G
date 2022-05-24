3ee287daaa2ac9ec9a47031d43aa4d7329cbeaed
yejunho10 <hyjcompany30@naver.com>
2022-05-24 18:38:13 +0900

    [액티브] 점멸 (Flash) - 완성

---
+++ src/main/java/com/dpnw/rtrpg/skills/skillActive/Flash.java
@@ -0,0 +1,51 @@
+package com.dpnw.rtrpg.skills.skillActive;
+
+import com.dpnw.rtrpg.enums.Rank;
+import com.dpnw.rtrpg.enums.SkillName;
+import com.dpnw.rtrpg.rplayer.obj.RPlayer;
+import com.dpnw.rtrpg.skills.obj.RActive;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
+
+/*
+[액티브] 점멸 (Flash)
+Unlock : 상점에서 구매
+
+Use : 바라보고 있는 방향으로 4블럭 텔레포트 한다.
+
+Range : 4
+Require mana : 0
+Cooldown : 20
+Rank : Uncommon
+Visible : False
+ */
+public class Flash extends RActive {
+    public Flash(Player p) {
+        setRange(4);
+        setRequireMana(0);
+        setCooldown(20);
+        setRank(Rank.UNCOMMON);
+        setVisible(false);
+        setSkillName(SkillName.FLASH);
+    }
+
+    @Override
+    public String skillUnlockCondition() {
+        return "상점에서 구매";
+    }
+
+    @Override
+    public void use(RPlayer p) {
+        Location loc = p.getPlayer().getLocation();
+        Vector dir = loc.getDirection();
+        dir.normalize();
+        dir.multiply(4); //4블럭 텔레포트
+        loc.add(dir);
+        p.getPlayer().teleport(loc);
+    }
+
+    @Override
+    public void cancel() {
+    }
+}
