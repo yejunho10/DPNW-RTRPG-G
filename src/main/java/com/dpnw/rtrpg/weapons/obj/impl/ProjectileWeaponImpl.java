d02492bd6bb252a159f587184ad1a7c40e1b2abc
darksoldier1404 <darksoldier1404@gmail.com>
2022-05-26 20:35:44 +0900

    무기 구조 수정

--- src/main/java/com/dpnw/rtrpg/weapons/obj/impl/ProjectileWeaponImpl.java
+++ src/main/java/com/dpnw/rtrpg/weapons/obj/impl/ProjectileWeaponImpl.java
@@ -1,12 +1,5 @@
 package com.dpnw.rtrpg.weapons.obj.impl;
 
-import org.bukkit.entity.Projectile;
+public abstract class ProjectileWeaponImpl extends WeaponImpl {
 
-public class ProjectileWeaponImpl extends WeaponImpl{
-    private Projectile projectile;
-
-    @Override
-    public void use() {
-
-    }
 }
