707c586e8b37fdcacbc21e773ee06d2e082e44c0
yejunho10 <hyjcompany30@naver.com>
2022-05-27 17:02:26 +0900

    Weapons(STAFF, CROSSBOW) dev #5

---
+++ src/main/java/com/dpnw/rtrpg/weapons/ExpertStaff.java
@@ -0,0 +1,19 @@
+package com.dpnw.rtrpg.weapons;
+
+import com.dpnw.rtrpg.enums.Rank;
+import com.dpnw.rtrpg.weapons.melee.obj.DualSword;
+
+@SuppressWarnings("all")
+public class ExpertStaff extends DualSword {
+    public ExpertStaff() {
+        setRank(Rank.COMMON);
+        setDefaultWeaponDamage(67);
+        setDefaultWeaponAttackSpeed(1);
+        setDefaultWeaponCritChance(10);
+        setDefaultWeaponCritDamage(1.5);
+        setIncreaseHealth(37);
+        setIncreaseMana(85);
+        setIncreaseManaRegen(0.8);
+        setFlavorText("미끌미끌 끈적끈적 기분나쁜 스태프다. 도대체 이게 왜 슬라임의 뱃속에 들어 있는거지?");
+    }
+}
