4c153c2fc8a5d7d55482c000f5618437a3132ebc
Bruce0203 <BruceFreedy@gmail.com>
2022-05-31 22:20:12 +0900

    add cancel on effectBegin and damageEntity to sleep effect

--- src/main/java/com/dpnw/rtrpg/skilleffect/impl/HandleSleep.java
+++ src/main/java/com/dpnw/rtrpg/skilleffect/impl/HandleSleep.java
@@ -1,9 +1,11 @@
 package com.dpnw.rtrpg.skilleffect.impl;
 
 import com.dpnw.rtrpg.skilleffect.HandleEntity;
+import com.dpnw.rtrpg.skilleffect.event.EffectBeginEvent;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 
 public class HandleSleep extends HandleStun {
@@ -14,6 +16,20 @@ public class HandleSleep extends HandleStun {
         if (event.getEntity() instanceof LivingEntity
                 && testEntity((LivingEntity) event.getEntity()))
             event.setDamage(event.getDamage() * 1.5);
+    }
+
+    @EventHandler(priority = EventPriority.LOW)
+    public void onBeginEffectSleep(EffectBeginEvent event) {
+        if (event.getEntity() instanceof LivingEntity entity && testEntity(entity)) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onEntityDamageByEntitySleep(EntityDamageByEntityEvent event) {
+        if (event.getDamager() instanceof LivingEntity entity && testEntity(entity)) {
+            event.setCancelled(true);
+        }
     }
 
 }
