2096834e11c04adb70cd69bc8f837ff8cc1ad672
Bruce0203 <BruceFreedy@gmail.com>
2022-06-03 21:38:42 +0900

    remove effect on damaged while effected "sleep"

--- src/main/java/com/dpnw/rtrpg/skilleffect/effect/HandleSleep.java
+++ src/main/java/com/dpnw/rtrpg/skilleffect/effect/HandleSleep.java
@@ -6,6 +6,7 @@ import com.dpnw.rtrpg.skilleffect.entity.SEntity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 
 public class HandleSleep extends EffectEntityImpl<SEntity<?>>
         implements NoBeginEffect, NoAttack, NoMove, MultiplyDamage {
@@ -15,5 +16,15 @@ public class HandleSleep extends EffectEntityImpl<SEntity<?>>
 
     @Override
     public double getMultiplier() { return 1.5; }
+
+    @EventHandler
+    @Override
+    public void modifyDamage(EntityDamageEvent event) {
+        if (event.getEntity() instanceof LivingEntity entity && testEntity(entity))  {
+            removeEffect(getHandleEntity().getEntity(entity));
+            event.setDamage(event.getDamage() * getMultiplier());
+        }
+
+    }
 
 }
